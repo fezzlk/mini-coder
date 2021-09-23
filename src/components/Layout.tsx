94fcf680737a12223619cf6348b90d9eb293cf7e
fezzlk <fezzlk796@gmail.com>
2021-09-23 15:57:49 +0900

    create and apply src dir

---
+++ src/components/Layout.tsx
@@ -0,0 +1,23 @@
+// import Head from 'next/head'
+// import styles from '../styles/Home.module.css'
+
+// type Props = {
+//   children?: React.ReactNode;
+// };
+
+// export default function Layout({ children }: Props) {
+//   return (
+//     <div className={styles.container}>
+//       <Head>
+//         <title>mini-corder</title>
+//         <meta name="description" content="mini-coder help you to practice coding!" />
+//         <link rel="icon" href="/favicon.ico" />
+//       </Head>
+
+//       <main className={styles.main}>{children}</main>
+
+//       <footer className={styles.footer}>
+//       </footer>
+//     </div>
+//   );
+// }

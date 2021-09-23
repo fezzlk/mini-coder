375653481b27d0f7b95d9a95fb5736248b562c7b
fezzlk <fezzlk796@gmail.com>
2021-09-23 16:19:02 +0900

    apply Layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,23 +1,23 @@
-// import Head from 'next/head'
-// import styles from '../styles/Home.module.css'
+import Head from 'next/head'
+import styles from '~/styles/Home.module.css'
 
-// type Props = {
-//   children?: React.ReactNode;
-// };
+type Props = {
+  children?: React.ReactNode;
+};
 
-// export default function Layout({ children }: Props) {
-//   return (
-//     <div className={styles.container}>
-//       <Head>
-//         <title>mini-corder</title>
-//         <meta name="description" content="mini-coder help you to practice coding!" />
-//         <link rel="icon" href="/favicon.ico" />
-//       </Head>
+export default function Layout({ children }: Props) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>mini-corder</title>
+        <meta name="description" content="mini-coder help you to practice coding!" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-//       <main className={styles.main}>{children}</main>
+      <main className={styles.main}>{children}</main>
 
-//       <footer className={styles.footer}>
-//       </footer>
-//     </div>
-//   );
-// }
+      <footer className={styles.footer}>
+      </footer>
+    </div>
+  );
+}

8cd01cf109fdf4a817872fc8014a8fbf3d4e0036
fezzlk <fezzlk796@gmail.com>
2021-10-12 01:29:00 +0900

    get question sentence from server

--- frontend/src/pages/exam.tsx
+++ frontend/src/pages/exam.tsx
@@ -1,14 +1,29 @@
 import type { NextPage } from 'next';
-import { useState } from 'react';
+import axiosBase from 'axios';
+import useSWR from 'swr';
 import Button from '@mui/material/Button';
 import Editor from "@monaco-editor/react";
 
 const ExamPage: NextPage = () => {
+  const axios = axiosBase.create({
+    baseURL: 'http://localhost:3100',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Requested-With': 'XMLHttpRequest',
+    },
+    responseType: 'json',
+  });
+  axios.defaults.withCredentials = true; 
+  const fetcher = (url: string) => axios.get(`${url}`);
+  const { data, error } = useSWR('/1', fetcher)
 
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
+  console.log(data);
   return (
     <div className="grid grid-cols-1 gap-5 my-10">
       <div>
-        Q. 整数 a, b が標準入力で与えられます。a + b を出力してください。
+        Q. {data.data}
       </div>
       <div className="border">
         <Editor

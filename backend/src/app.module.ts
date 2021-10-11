8ef156f6718936636e0f7cfdd70bfb66a7c59d57
fezzlk <fezzlk796@gmail.com>
2021-10-12 01:28:24 +0900

    create and apply cors middleware

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { CorsMiddleware } from './middlewares/cors';
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): MiddlewareConsumer | void {
+    consumer.apply(CorsMiddleware).forRoutes('*');
+  }
+}

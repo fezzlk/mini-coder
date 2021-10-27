3797bfad81313d7e62a9e7754006f0ff7bd775c3
fezzlk <fezzlk796@gmail.com>
2021-10-28 08:30:51 +0900

    問題文データの取得の流れを修正

--- frontend/src/components/ExamArea.tsx
+++ frontend/src/components/ExamArea.tsx
@@ -1,31 +1,19 @@
-import axiosBase from 'axios';
-import useSWR from 'swr';
+import { useEffect, useState } from 'react';
+// import useSWR from 'swr';
 import { useQuestionSet } from '~/stores/contexts';
 import Button from '@mui/material/Button';
 import Editor from "@monaco-editor/react";
 
 
 export default function ExamArea() {
-  const axios = axiosBase.create({
-    baseURL: 'http://localhost:3100',
-    headers: {
-      'Content-Type': 'application/json',
-      'X-Requested-With': 'XMLHttpRequest',
-    },
-    responseType: 'json',
-  });
-  axios.defaults.withCredentials = true; 
-  const fetcher = (url: string) => axios.get(`${url}`);
-  // const { data, error } = useQuestionSet(fetcher);
-  const { data, error } = useSWR('/1', fetcher);
-
-  if (error) return <div>問題の取得に失敗しました。</div>
-  if (!data) return <div>Loading...</div>
+  const { data: questionSet, mutate: mutateQuestionSet } = useQuestionSet();
+  // if (error) return <div>問題の取得に失敗しました。</div>
+  // if (!data) return <div>Loading...</div>
 
   return (
     <div className="grid grid-cols-1 gap-5 my-10">
       <div>
-        Q. {data.data}
+        Q. {questionSet}
       </div>
       <div className="border">
         <Editor
--- frontend/src/pages/exam.tsx
+++ frontend/src/pages/exam.tsx
@@ -1,13 +1,43 @@
 import type { NextPage } from 'next';
 import ExamArea from '~/components/ExamArea';
-import Layout from '~/components/Layout'
+import Layout from '~/components/Layout';
+import { useQuestionSet } from '~/stores/contexts';
+import axiosBase from 'axios';
 
-const ExamPage: NextPage = () => {
+const ExamPage: NextPage = (props) => {
+  const { data: questionSet, mutate: mutateQuestionSet } = useQuestionSet();
+  mutateQuestionSet(props.data)
+  console.log(props.data)
   return (
     <Layout>
       <ExamArea />
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const axios = axiosBase.create({
+    baseURL: 'http://localhost:3100',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Requested-With': 'XMLHttpRequest',
+    },
+    responseType: 'json',
+  });
+  axios.defaults.withCredentials = true;
+  const { data } = await axios.get(`/1`);
+  console.log(data);
+  if (data == null) {
+    return {
+      props: {
+        data: '問題の取得に失敗しました。'
+      }
+    }
+  }
+    
+  return {
+    props: { data },
+  }
 }
 
 export default ExamPage
